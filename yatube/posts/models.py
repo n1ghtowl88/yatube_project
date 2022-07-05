f2046b56c3a3e6adc4d7c7cb0390238208ddccd9
Eldar <eldar.6al@gmail.com>
2022-07-05 12:08:55 +0300

    Add Post and Group models

---
+++ yatube/posts/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+# Create your models here.
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200
+    )
+    slug = models.SlugField(
+        unique=True
+    )
+    description = models.TextField(
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField(
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    def __str__(self):
+        return f'{self.author.username}, {self.group} - {self.text[:15]}'
---
+++ yatube/posts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
+
+
+def index(request):
+    title = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts
+    }
+    return render(request, 'posts/group_list.html', context)
