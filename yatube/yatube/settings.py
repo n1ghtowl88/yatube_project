310807f6696611b88ef1b561d0945628e774528b
Eldar Galimshin <63157399+n1ghtowl88@users.noreply.github.com>
2022-08-11 20:45:21 +0300

    Update settings.py

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ez6(_y583y_ufobi5cq@_&_t-z8@c0^hek+cq454iehjm2j56x'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
